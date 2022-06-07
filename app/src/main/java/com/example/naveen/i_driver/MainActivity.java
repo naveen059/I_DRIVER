22a1a02477aac69389295cf80b9b07bda9c77e3e
navee <naveenkmacharya@gmail.com>
2022-06-07 20:01:20 +0530

    initial commit

--- app/src/main/java/com/example/naveen/i_driver/MainActivity.java
+++ app/src/main/java/com/example/naveen/i_driver/MainActivity.java
@@ -9,18 +9,10 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button cont_btn = findViewById(R.id.cont_btn);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        cont_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Activity_otp.class);
-                startActivity(intent);
-            }
-        });
     }
 }
